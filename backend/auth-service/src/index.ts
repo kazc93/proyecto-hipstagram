35cd0a4edd3eda82724385cd0b4e274149293b4a
6mmario <mmario_93@hotmail.es>
2026-06-13 16:20:01 -0600

    Update log message to include 'de 6mmario'

--- backend/auth-service/src/index.ts
+++ backend/auth-service/src/index.ts
@@ -2,5 +2,5 @@ import app from './app';
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`🚀 Auth Service corriendo en puerto ${PORT}`);
+    console.log(`🚀 Auth Service corriendo en puerto ${PORT} de 6mmario`);
 });
